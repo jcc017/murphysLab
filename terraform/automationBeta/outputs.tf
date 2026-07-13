50b5162a0097db106c89566f53e0b53528209992
jcc017 <clarkjoshua499@gmail.com>
2026-07-13 15:57:35 -0400

    moving beta automation project to different directory

--- terraform/automationBeta/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "win_prv_ip" {
-  description = "Private IP address of new Windows Server"
-  value       = aws_instance.win_srv.private_ip
-}
-
-output "unix_prv_ip" {
-  description = "Private IP address of new Unix Server"
-  value       = aws_instance.unix_srv.private_ip
-}
